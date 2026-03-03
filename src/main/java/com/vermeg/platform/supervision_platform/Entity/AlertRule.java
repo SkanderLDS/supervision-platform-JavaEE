c52dda244eb1c1644e94494f0b6eb5318c6c7053
SkanderLDS <skanderlandolsi29@yahoo.fr>
2026-03-04 00:56:00 +0900

    ajouter réglages des alertes(alert rules)

---
+++ src/main/java/com/vermeg/platform/supervision_platform/Entity/AlertRule.java
@@ -0,0 +1,43 @@
+package com.vermeg.platform.supervision_platform.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "alert_rules")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AlertRule {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AlertRuleType type;
+
+    @Column(nullable = false)
+    private double threshold;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AlertLevel level;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private boolean enabled = true;
+
+    @Column(nullable = false, updatable = false)
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+
+
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "server_id", nullable = false)
+    private Server server;
+}
---
+++ src/main/java/com/vermeg/platform/supervision_platform/Entity/AlertRuleType.java
@@ -0,0 +1,8 @@
+package com.vermeg.platform.supervision_platform.Entity;
+
+public enum AlertRuleType {
+    CPU_USAGE,
+    MEMORY_USAGE,
+    DISK_USAGE,
+    ERROR_COUNT_PER_MINUTE
+}
